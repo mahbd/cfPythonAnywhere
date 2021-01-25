7c6def25c8244e8702d59ce127c4cfc55d7990b9
Mahmudul Alam <mahmudula2000@gmail.com>
2021-01-25 22:26:54 +0600

    Improved admin

--- cf/models.py
+++ cf/models.py
@@ -6,18 +6,12 @@ class Handle(models.Model):
     handle = models.CharField(blank=True, null=True, max_length=200)
     batch = models.TextField(blank=True, null=True)
 
-    def __str__(self):
-        return self.handle
-
 
 class Problems(models.Model):
     name = models.CharField(max_length=255, blank=True, null=True, unique=True)
     link = models.TextField(blank=True, null=True)
     solver = models.ManyToManyField(Handle, blank=True)
     num_sol = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.name
 
     class Meta:
         ordering = ['-num_sol']
